0531d27d5a8a35c9c49636d9981f62dcf620cf8d
Vlad <example@gmail.com>
2022-09-07 10:41:50 +0300

    project_2 deistvia na stranitse

--- js/script1.js
+++ js/script1.js
@@ -1,39 +1,59 @@
-"use strict"
-
-for (let i = 0; i < 3; i++){
-    console.log(i);
-    for (let j = 0; j < 3; j++){
-        console.log(j);
-
-    }
-}
+'use strict';
 
 
-// *
-// **
-// ***
-// ****
-// *****
+const   box = document.getElementById('box'),
+        btns = document.getElementsByTagName('button'),
+        circles = document.getElementsByClassName('circle'),
+        wrapper = document.querySelector('.wrapper'),
+        hearts = wrapper.querySelectorAll('.heart'),
+        oneHeart = wrapper.querySelector('.heart');
+       
+        
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
 
-let result = '';
-const lenght = 7;
+box.style.cssText = `background-color: blue; width: 500px`;
 
-for (let i = 1; i < lenght; i++){
-    for (let j = 0; j < i; j++){
-        result += "*";
-    }
-    result += '\n';
-}
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-console.log(result);
+// for (let i = 0; i < hearts.length; i++){
+//     hearts[i].style.backgroundColor = 'blue';
+// }
 
-first: for (let i = 0; i < 3; i++){
-    console.log(`First lvl: ${i}`);
-    for (let j = 0; j < 3; j++){
-        console.log(`Second lvl: ${j}`);
-        for (let k = 0; k < 3; k++){    
-            if (k ===2 ) break first;
-            console.log(`Third lvl: ${k}`);
-        }
-    }
-}
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+
+//модификация css классов
+div.classList.add('black');
+
+// wrapper.appendChild('div');//ustarevsh
+
+
+wrapper.append(div); //помещает в конец родителя элемент
+// wrapper.prepend(div); //помещает в конец родителя жлемент
+// hearts[0].before(div); //do
+// hearts[0].after(div); // posle
+
+// wrapper.insertBefore(div, hearts[1]); //ustar
+
+// circles[0].remove(); //udalenie elementa
+// wrapper.removeChild(hearts[1]); //ustar
+
+hearts[0].replaceWith(circles[0]); //samena elementa
+// wrapper.replaceChild(circles[0], hearts[0]);//ustar
+
+
+//как добалять элемеенты в html
+//пвисать текст
+//1) inner html 
+div.innerHTML = "<h1>Hello world</h1>";
+//2)text content только с текстом работает, без html струтуры
+// div.textContent = "hello";
+
+
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
